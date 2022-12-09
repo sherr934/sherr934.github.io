fc9e1da70996e8c23b4082fa53ca82c34df0165e
sherr934 <sherr934@newschool.edu>
2022-12-09 17:46:55 -0500

    entry 9
    finished entry 9

--- entries/entry9/main.js
+++ entries/entry9/main.js
@@ -1,1 +1,10 @@
-console.log("hello!");
+
+let body = document.body;
+
+function turnOnLight() {
+  body.classList.add("light");
+  button.classList.add("buttonOn");
+}
+
+let button = document.querySelector(".button");
+button.addEventListener("click", turnOnLight);
